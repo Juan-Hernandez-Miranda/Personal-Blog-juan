2532381802b8269baf3367fb3341b51beca69745
Juan-Hernandez-Miranda <A24216403@alumnos.uady.mx>
2026-02-02 01:49:51 -0600

    comprobacion de la conexion a la abse de datos

--- blog_entero_python/Backend/db/database.py
+++ blog_entero_python/Backend/db/database.py
@@ -18,10 +18,10 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 
-DATABASE_URL = { 
+DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-}
+)
 
 
 engine = create_engine(DATABASE_URL, echo=True)
--- blog_entero_python/Backend/routers/log_in.py
+++
@@ -1,4 +0,0 @@
-"""
-en este vamos a usar este router para hacer el login, o mas bein la logica detras del login
-"""
- 
